897f4502f4dec403b5e049262944ca27c503f5ba
KlimLe <167296963+KlimLe@users.noreply.github.com>
2024-06-27 14:06:40 +0200

    Update streamlitRun.py

--- streamlitRun.py
+++ streamlitRun.py
@@ -4,7 +4,7 @@ import gdown
 import streamlit as st
 
 # Download the model from Google Drive
-file_id = '1L69so1We0E67X0bRHSOkEsvPYeSIEnr6'
+file_id = '1iGO6l1e6zgGFKRVmwHkxNn4f0kuNBAF6'
 url = f'https://drive.google.com/uc?export=download&id={file_id}'
 output = 'trained_model.h5'
 gdown.download(url, output, quiet=False)
